983320bc5c577c0c3124db997d62db737bdc8e95
mkpappu <mkpappu1@gmail.com>
2024-01-16 13:20:45 -0500

    v7 wrapper for index.php

--- index.php
+++ index.php
@@ -7,25 +7,22 @@ require_once 'exp/conf.php';
 <html>
 
 <head>
-  <!-- add the title of the experiment that would be seen in the browser -->
   <title><?php echo $experimentName; ?></title>
   <!-- PHP wrapper libraries -->
   <script type="text/javascript" src="db/validate.js"></script>
-  <script type="text/javascript" src="jQuery/jquery-3.5.1.min.js"></script>
-  <!-- jsPsych library -->
-  <script type="text/javascript" src="jsPsych/jspsych.js"></script>
+  <script type="text/javascript" src="db/jquery-3.5.1.min.js"></script>
+  <!-- jsPsych CDN libraries -->
+  <script src="https://unpkg.com/jspsych@7.3.3"></script>
+  <link href="https://unpkg.com/jspsych@7.3.3/css/jspsych.css" rel="stylesheet" type="text/css"/>
   <!-- jsPsych Plugins (add more here) -->
-  <script type="text/javascript" src="jsPsych/plugins/jspsych-html-keyboard-response.js"></script>
-  <script type="text/javascript" src="jsPsych/plugins/jspsych-image-keyboard-response.js"></script>
-  <script type="text/javascript" src="jsPsych/plugins/jspsych-video-keyboard-response.js"></script>
-  <link href="jsPsych/css/jspsych.css" rel="stylesheet" type="text/css">
+  <script src="https://unpkg.com/@jspsych/plugin-html-keyboard-response@1.1.2"></script>
   </link>
   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body id='unload' onbeforeunload="return areYouSure()">
 <?php
-    if (isset($_GET["workerId"]) || isset($_GET["PROLIFIC_PID"]) || isset($_GET["participantId"])) {
+    if ($_GET["workerId"] || $_GET["PROLIFIC_PID"]) {
       switch ($language) {
         case 'english':
           include_once "include/consent/english.php";
@@ -39,7 +36,7 @@ require_once 'exp/conf.php';
           include_once "include/consent/german.php";
           break;
         }
-    } else if (isset($_GET["src_subject_id"])) {
+    } else if ($_GET["src_subject_id"]) {
       include_once "include/nda.php";
     } else {
       include_once "include/intake.php";
@@ -47,8 +44,9 @@ require_once 'exp/conf.php';
   ?>
 </body>
 <footer>
-  <script type="text/javascript" src="exp/lang.js"></script>
+  <!-- load experiment dependencies -->
   <script type="text/javascript" src="exp/fn.js"></script>
+  <script type="text/javascript" src="exp/lang.js"></script>
   <script type="text/javascript" src="exp/var.js"></script>
   <script>
     // show page when loaded 
