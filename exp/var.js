f5cbc6fe43905107a8687bc8172090062b4e93a3
Praveen Suthaharan <praveen.suthaharan.github@gmail.com>
2024-01-23 15:55:34 -0500

    starting setting up prl game strategy in var.js with Josh, need to work on finding the correct randomization function that ensures the randomization does not produce the same set as the previous set

--- exp/var.js
+++ exp/var.js
@@ -1,6 +1,8 @@
 // Purpose of var.js: To include all global variables (e.g., trialIterator) 
 let trialIterator = 1;
 let stim;
+let contingency;
+let probability;
 
 //selecting the stimuli based on version (deck or avatar)
 if (version === "deck") {
@@ -10,46 +12,52 @@ if (version === "avatar") {
     stim = stimArrayAvatar;
 }
 
+stimRandomize = jsPsych.randomization.repeat(stim,1);
 
-// /*define stroop_variables(stroop stimuli)*/
-// const first40 = [
-//     {
-//         left: deckPositions[0],
-//         middle: deckPositions[1],
-//         right: deckPositions[2],
-//         fixedProbabilities: [0.9, 0.5, 0.1],
-//         probabilityNames: ["high", "medium", "low"],
-//     },
-// ];
+contingency = ["high","medium","low"];
+probability = [0.9,0.5,0.1];
+let rewardProbabilityFirstHalf = [
+    {
+        contingency: contingency[0],
+        probability: probability[0],
+        deck: stimRandomize
+    },
 
-// const second40 = [
-//     {
-//         left: leftStim,
-//         middle: middleStim,
-//         right: rightStim,
-//         probabilities: [0.5, 0.1, 0.9],
-//         probabilityNames: ["medium", "low", "high"],
-//     },
-// ];
+    {
+        contingency: contingency[1],
+        probability: probability[1],
+        deck: stimRandomize
+    },
 
-// // probabilities change at second half
+    {
+        contingency: contingency[2],
+        probability: probability[2],
+        deck: stimRandomize
+    },
+];
 
-// const third40 = [
-//     {
-//         left: leftStim,
-//         middle: middleStim,
-//         right: rightStim,
-//         probabilities: [0.8, 0.4, 0.2],
-//         probabilityNames: ["high", "medium", "low"],
-//     },
-// ];
+let rewardProbabilityRandomizeFirstHalf = jsPsych.randomization.repeat(rewardProbabilityFirstHalf,1);
 
-// const fourth40 = [
-//     {
-//         left: leftStim,
-//         middle: middleStim,
-//         right: rightStim,
-//         probabilities: [0.2, 0.4, 0.8],
-//         probabilityNames: ["low", "medium", "high"],
-//     },
-// ];
+contingency = ["high","medium","low"];
+probability = [0.8,0.4,0.2];
+let rewardProbabilitySecondHalf = [
+    {
+        contingency: contingency[0],
+        probability: probability[0],
+        deck: stimRandomize
+    },
+
+    {
+        contingency: contingency[1],
+        probability: probability[1],
+        deck: stimRandomize
+    },
+
+    {
+        contingency: contingency[2],
+        probability: probability[2],
+        deck: stimRandomize
+    },
+];
+
+let rewardProbabilityRandomizeSecondHalf = jsPsych.randomization.repeat(rewardProbabilitySecondHalf,1);
