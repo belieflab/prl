a688d381d071ec94e1b6f22290f1075efbd45f31
joshkenney <joshuagkenney@gmail.com>
2024-01-27 18:00:49 -0500

    magicword

--- redirect.php
+++ redirect.php
@@ -35,7 +35,7 @@ if ($workerId) {
     // Redirect to index.php with the workerId parameter
     echo"<h1>Ah! Ah! Ah!<h1>";
     echo"<h1>You didn't say the magic word!<h1/>";
-    echo"<img src='magicword.gif'>";
+    echo"<img src='wrap/magicword.gif'>";
     exit; // Make sure to exit after the header redirect
 }
 
